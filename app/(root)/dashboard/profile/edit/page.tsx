b1b5778c6099123ebae23f4343cfe41e4665be0d
adam <adamg42685@gmail.com>
2024-01-30 14:23:01 -0500

    fixing types on edit page

--- app/(root)/dashboard/profile/edit/page.tsx
+++ app/(root)/dashboard/profile/edit/page.tsx
@@ -4,43 +4,27 @@ import { IUser } from "@/database/models/user.model";
 import { getUserInfo } from "@/lib/actions/users.actions";
 import { currentUser } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs";
-import { error } from "console";
 import React from "react";
 
-interface Props {
-  clerkId?: string;
-  user: {
-    _id: string;
-    clerkId: string;
-    picture: string;
-    name: string;
-    username: string;
-    googleId?: string;
-    appleId?: string;
-  };
-}
-
-const Page = async ({ user, clerkId }: Props) => {
+const Page = async () => {
   const { userId } = auth();
 
   if (!userId) return null;
 
   const mongoUser: IUser = await getUserInfo({ userId });
   if (!mongoUser) return null;
-  if (!user) {
-    console.log(mongoUser);
-    try {
-      const response = await fetch(`http://localhost:3000/api/users/`);
-      const user = await response.json();
-      console.log(user);
-    } catch (error) {}
-  }
+
   return (
     <>
       <h1 className="h1-bold text-dark100_light900">Edit Profile</h1>
 
       <div className="mt-9">
-        {userId && <ProfileCard clerkId={userId} user={user} />}
+        {userId && (
+          <ProfileCard
+            clerkId={userId}
+            user={JSON.parse(JSON.stringify(mongoUser!))}
+          />
+        )}
       </div>
       <div>
         {userId && (
